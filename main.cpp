023d2f1088c3cf00235174e153ce410ed962a011
Jonathan <cassenjonathan@gmail.com>
2013-12-15 16:10:35 +0100

    Poly and Window

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
-
 #include <iostream>
 #include <glut.h>
 #define WIDTH 600
 #define HEIGHT 600
 
 double arr[5000][4];
+double arrWin[2][2] = {{0,0},{WIDTH,HEIGHT}};
 int z=0;
+int z2=0;
 int flag2=0;
+int fmen = 0;
 float radius=0.03;
 float color[3][3]={{1.0,1.0,1.0},{1.0,1.0,0.0},{0.0,1.0,0.0}};
 
 enum MENU_TYPE
 {
-        MENU_FRONT,
-        MENU_SPOT,
+        MENU_POL,
+        MENU_WIN,
+        MENU_APP,
         MENU_BACK,
         MENU_BACK_FRONT,
 };
 
 MENU_TYPE show = MENU_BACK_FRONT;
-
-void menu(int);
 
 void init()
 {
@@ -57,9 +58,21 @@ void drawPoints()
     glEnd();
 }
 
-void drawLines()
+void drawRect()
 {
+    glColor3ub(0,0,255);
     glBegin(GL_LINE_LOOP);
+    glVertex2f(arrWin[0][0],arrWin[0][1]);
+    glVertex2f(arrWin[1][0],arrWin[0][1]);
+    glVertex2f(arrWin[1][0],arrWin[1][1]);
+    glVertex2f(arrWin[0][0],arrWin[1][1]);
+    glEnd();
+}
+
+void drawPoly()
+{
+    glColor3ub(255,0,0);
+    glBegin(GL_POLYGON);
     for(int i=0;i<z;i++)
     {
         glVertex2f(arr[i][0],arr[i][1]);
@@ -67,21 +80,33 @@ void drawLines()
     glEnd();
 }
 
-
 void addValue(int x,int y)
 {
     arr[z][0]=getOpenGLX(x);
     arr[z++][1]=getOpenGLY(y);
 }
 
+void addWinValue(int x,int y)
+{
+    if(z2 == 0){
+        arrWin[0][0]=getOpenGLX(x);
+        arrWin[0][1]=getOpenGLY(y);
+        z2 = 1;
+    }
+    else if(z2 == 1){
+        arrWin[1][0]=getOpenGLX(x);
+        arrWin[1][1]=getOpenGLY(y);
+        z2 = 0;
+    }
+
+}
+
 
 void myDisplay()
 {
     glClear( GL_COLOR_BUFFER_BIT);
-    drawLines();
-    if(flag2){
-        drawPoints();
-    }
+    drawPoly();
+    drawRect();
     glutSwapBuffers();
     glutPostRedisplay();
     glFlush();
@@ -92,21 +117,15 @@ void myMouseStat(int button,int state,int x, int y)
 {
     if(button==GLUT_RIGHT_BUTTON && state==GLUT_DOWN)
     {
-        //resetAll();
+        //Right Click
     }
     if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_DOWN))
     {
-        if(!flag2){
-            flag2=1;
+        if(fmen == 0){
             addValue(x,y);
         }
-    }
-
-    else if ((button == GLUT_LEFT_BUTTON) && (state == GLUT_UP))
-    {
-        if(flag2){
-            flag2=0;
-
+        else if(fmen == 1){
+            addWinValue(x,y);
         }
     }
 
@@ -116,24 +135,23 @@ void menu(int item)
 {
         switch (item)
         {
-        case MENU_FRONT:
-            glClear( GL_COLOR_BUFFER_BIT );
-            glColor3ub(255,0,0);
+        case MENU_POL:
+            fmen = 0;
             break;
-        case MENU_SPOT:
-            glClear( GL_COLOR_BUFFER_BIT );
-            glColor3ub(0,0,255);
+        case MENU_WIN:
+            fmen = 1;
             break;
         case MENU_BACK:
             resetAll();
+            break;
+        case MENU_APP:
+            fmen = 3;
+            break;
         case MENU_BACK_FRONT:
-                {
-                        show = (MENU_TYPE) item;
-                }
-                break;
+            show = (MENU_TYPE) item;
+            break;
         default:
-                {       /* Nothing */       }
-                break;
+            break;
         }
 
         glutPostRedisplay();
@@ -152,10 +170,13 @@ int main( int argc, char ** argv)
     glutDisplayFunc(myDisplay);
     glutMouseFunc(myMouseStat);
 
+    glClear( GL_COLOR_BUFFER_BIT );
+    glColor3ub(255,0,0);
 
     glutCreateMenu(menu);
-    glutAddMenuEntry("Change To Polygon", MENU_FRONT);
-    glutAddMenuEntry("Change To Window", MENU_SPOT);
+    glutAddMenuEntry("Change To Polygon", MENU_POL);
+    glutAddMenuEntry("Change To Window", MENU_WIN);
+    glutAddMenuEntry("Survey", MENU_APP);
     glutAddMenuEntry("Reset", MENU_BACK);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 
